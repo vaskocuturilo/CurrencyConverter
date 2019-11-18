56d63f7009ab40ba12e61623cc308fae1e92645c
Vasko <vaskocuturilo@rambler.ru`>
2019-11-18 14:58:21 +0300

    Feat : WebDriverProvider : Added JavaDoc.
    Commit at 18/11/2019

--- src/main/java/com/oanda/browser/WebDriverProvider.java
+++ src/main/java/com/oanda/browser/WebDriverProvider.java
@@ -10,10 +10,15 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public interface WebDriverProvider {
 
     /**
-     * Create driver web driver.
+     * Create new {@link WebDriver} instance. The instance will be bound to current thread, so there is no need to cache
+     * this instance in method implementation. Also don't cache the instance in static variable, as <a
+     * href="http://code.google.com/p/selenium/wiki/FrequentlyAskedQuestions#Q:_Is_WebDriver_thread-safe?">WebDriver
+     * instance is not thread-safe</a>.
      *
-     * @param desiredCapabilities the desired capabilities
-     * @return the web driver
+     * @param desiredCapabilities set of desired capabilities as suggested by Selenium framework;
+     *                            method implementation is recommended to pass this variable to {@link WebDriver},
+     *                            probably modifying it according to specific needs.
+     * @return new {@link WebDriver} instance.
      */
     WebDriver createDriver(DesiredCapabilities desiredCapabilities);
 }
