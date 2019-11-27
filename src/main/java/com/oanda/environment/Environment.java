a348301a6e6366db3a9d44da0e9bfd37325de011
Vasko <vaskocuturilo@rambler.ru`>
2019-11-27 13:12:57 +0300

    Fix : Environment : Fix method to isCheckOperatingSystem.
    Commit at 27/11/2019

--- src/main/java/com/oanda/environment/Environment.java
+++ src/main/java/com/oanda/environment/Environment.java
@@ -14,13 +14,13 @@ public final class Environment {
     }
 
     /**
-     * Method isCheckOSSystem.
+     * Method isCheckOperatingSystem.
      * If method return true, that is automation scripts to run on OS Unix or Linux.
      * If method return false, that is automation scripts to run on OS Windows or Mac.
      *
      * @return the boolean
      */
-    public static boolean isCheckOperationSystem() {
+    public static boolean isCheckOperatingSystem() {
 
         final String operationSystem = System.getProperty("os.name").toLowerCase(Locale.ROOT);
 
